7b20c9ea537b74f796e50afc4fd868685f3ddeb0
Code-Alexandre <alex.lapres@outlook.com>
2021-07-08 17:51:41 -0400

    deploy2

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,65 +1,65 @@
 class Lead < ApplicationRecord
     has_one_attached :file
-    after_create :ticket_create_contact
-    require 'zendesk_api'
+    # after_create :ticket_create_contact
+    # require 'zendesk_api'
 
-    def ticket_create_contact
-        client = ZendeskAPI::Client.new do |config|
-            # Mandatory:
+    # def ticket_create_contact
+    #     client = ZendeskAPI::Client.new do |config|
+    #         # Mandatory:
           
-            config.url = "https://rocketelevator8148.zendesk.com/api/v2" # e.g. https://rocketelevator8148.zendesk.com/api/v2
+    #         config.url = "https://rocketelevator8148.zendesk.com/api/v2" # e.g. https://rocketelevator8148.zendesk.com/api/v2
           
-            # Basic / Token Authentication
-            config.username = "rocketelevator@hotmail.com"
+    #         # Basic / Token Authentication
+    #         config.username = "rocketelevator@hotmail.com"
             
           
-            # Choose one of the following depending on your authentication choice
-            config.token = ENV["ZENDESK_APIKEY"]
+    #         # Choose one of the following depending on your authentication choice
+    #         config.token = ENV["ZENDESK_APIKEY"]
           
-          #   # OAuth Authentication
-          #   config.access_token = "your OAuth access token"
+    #       #   # OAuth Authentication
+    #       #   config.access_token = "your OAuth access token"
           
-            # Optional:
+    #         # Optional:
           
-            # Retry uses middleware to notify the user
-            # when hitting the rate limit, sleep automatically,
-            # then retry the request.
-            config.retry = true
+    #         # Retry uses middleware to notify the user
+    #         # when hitting the rate limit, sleep automatically,
+    #         # then retry the request.
+    #         config.retry = true
           
-            # Raise error when hitting the rate limit.
-            # This is ignored and always set to false when `retry` is enabled.
-            # Disabled by default.
-            config.raise_error_when_rate_limited = false
+    #         # Raise error when hitting the rate limit.
+    #         # This is ignored and always set to false when `retry` is enabled.
+    #         # Disabled by default.
+    #         config.raise_error_when_rate_limited = false
           
-            # Logger prints to STDERR by default, to e.g. print to stdout:
-            require 'logger'
-            config.logger = Logger.new(STDOUT)
+    #         # Logger prints to STDERR by default, to e.g. print to stdout:
+    #         require 'logger'
+    #         config.logger = Logger.new(STDOUT)
           
-            # Disable resource cache (this is enabled by default)
-            config.use_resource_cache = false
+    #         # Disable resource cache (this is enabled by default)
+    #         config.use_resource_cache = false
           
-            # Changes Faraday adapter
-            # config.adapter = :patron
+    #         # Changes Faraday adapter
+    #         # config.adapter = :patron
           
-            # Merged with the default client options hash
-            # config.client_options = {:ssl => {:verify => false}, :request => {:timeout => 30}}
+    #         # Merged with the default client options hash
+    #         # config.client_options = {:ssl => {:verify => false}, :request => {:timeout => 30}}
           
-            # When getting the error 'hostname does not match the server certificate'
-            # use the API at https://yoursubdomain.zendesk.com/api/v2
-        end
-        # ticket = ZendeskAPI::Ticket.new("#{client}", :id => 1, :priority => "urgent")
-        puts "This is the client after creation #{client}"
-        client.tickets.create(
-            :subject => "#{full_name} from #{company_name}", 
-            :comment => { :value => "The contact #{full_name} from company #{company_name} can be reached at email #{email} and at phone number #{phone_number}. #{departement} has a project named #{project_name} which would require contribution from Rocket Elevators.
-            #{project_description}
-            Attached Message: #{message}
-            The Contact uploaded an attachment" }, 
-            :submitter_id => client.current_user.id,
-            :type => "question", 
-            :priority => "normal")
-        puts "Ticket creation?"
-        client.tickets.save
-        puts "Ticket saved?"
-    end
+    #         # When getting the error 'hostname does not match the server certificate'
+    #         # use the API at https://yoursubdomain.zendesk.com/api/v2
+    #     end
+    #     # ticket = ZendeskAPI::Ticket.new("#{client}", :id => 1, :priority => "urgent")
+    #     puts "This is the client after creation #{client}"
+    #     client.tickets.create(
+    #         :subject => "#{full_name} from #{company_name}", 
+    #         :comment => { :value => "The contact #{full_name} from company #{company_name} can be reached at email #{email} and at phone number #{phone_number}. #{departement} has a project named #{project_name} which would require contribution from Rocket Elevators.
+    #         #{project_description}
+    #         Attached Message: #{message}
+    #         The Contact uploaded an attachment" }, 
+    #         :submitter_id => client.current_user.id,
+    #         :type => "question", 
+    #         :priority => "normal")
+    #     puts "Ticket creation?"
+    #     client.tickets.save
+    #     puts "Ticket saved?"
+    # end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,75 +1,75 @@
 class Quote < ApplicationRecord
-    after_create :ticket_create_quote
-    require 'zendesk_api'
+    # after_create :ticket_create_quote
+    # require 'zendesk_api'
 
-    def ticket_create_quote
-        client = ZendeskAPI::Client.new do |config|
-            # Mandatory:
+    # def ticket_create_quote
+    #     client = ZendeskAPI::Client.new do |config|
+    #         # Mandatory:
           
-            config.url = "https://rocketelevator8148.zendesk.com/api/v2" # e.g. https://rocketelevator8148.zendesk.com/api/v2
+    #         config.url = "https://rocketelevator8148.zendesk.com/api/v2" # e.g. https://rocketelevator8148.zendesk.com/api/v2
           
-            # Basic / Token Authentication
-            config.username = "rocketelevator@hotmail.com"
+    #         # Basic / Token Authentication
+    #         config.username = "rocketelevator@hotmail.com"
             
           
-            # Choose one of the following depending on your authentication choice
-            config.token = ENV["ZENDESK_APIKEY"]
+    #         # Choose one of the following depending on your authentication choice
+    #         config.token = ENV["ZENDESK_APIKEY"]
           
-          #   # OAuth Authentication
-          #   config.access_token = "your OAuth access token"
+    #       #   # OAuth Authentication
+    #       #   config.access_token = "your OAuth access token"
           
-            # Optional:
+    #         # Optional:
           
-            # Retry uses middleware to notify the user
-            # when hitting the rate limit, sleep automatically,
-            # then retry the request.
-            config.retry = true
+    #         # Retry uses middleware to notify the user
+    #         # when hitting the rate limit, sleep automatically,
+    #         # then retry the request.
+    #         config.retry = true
           
-            # Raise error when hitting the rate limit.
-            # This is ignored and always set to false when `retry` is enabled.
-            # Disabled by default.
-            config.raise_error_when_rate_limited = false
+    #         # Raise error when hitting the rate limit.
+    #         # This is ignored and always set to false when `retry` is enabled.
+    #         # Disabled by default.
+    #         config.raise_error_when_rate_limited = false
           
-            # Logger prints to STDERR by default, to e.g. print to stdout:
-            require 'logger'
-            config.logger = Logger.new(STDOUT)
+    #         # Logger prints to STDERR by default, to e.g. print to stdout:
+    #         require 'logger'
+    #         config.logger = Logger.new(STDOUT)
           
-            # Disable resource cache (this is enabled by default)
-            config.use_resource_cache = false
+    #         # Disable resource cache (this is enabled by default)
+    #         config.use_resource_cache = false
           
-            # Changes Faraday adapter
-            # config.adapter = :patron
+    #         # Changes Faraday adapter
+    #         # config.adapter = :patron
           
-            # Merged with the default client options hash
-            # config.client_options = {:ssl => {:verify => false}, :request => {:timeout => 30}}
+    #         # Merged with the default client options hash
+    #         # config.client_options = {:ssl => {:verify => false}, :request => {:timeout => 30}}
           
-            # When getting the error 'hostname does not match the server certificate'
-            # use the API at https://yoursubdomain.zendesk.com/api/v2
-        end
-        # ticket = ZendeskAPI::Ticket.new("#{client}", :id => 1, :priority => "urgent")
-        puts "This is the client after creation #{client}"
-        client.tickets.create(:subject => "#{company_name} at #{email}", 
-            :comment => { :value => "The company #{company_name} which can be reached at email #{email} sent us a quote requesting Rocket Elevators services. Here's the key elements of the quote we received:
-            Building type: #{name}
-            Product Line Quality: #{product_line}
-            Number of Apartments: #{number_of_appartement}
-            Number of Floors: #{number_of_floor}
-            Number of Basements: #{number_of_basement}
-            Number of Companies: #{number_of_companies}
-            Number of Parking Spots: #{number_of_parking_spots}
-            Number of Corporations: #{number_of_corporations}
-            Maximum Occupancy: #{maximum_occupancy}
-            Total Business Hours/Day: #{business_hours}
-            Amount of Elevator needed: #{number_of_elevators}
-            Unit Price per Elevator: #{unit_price_of_each_elevator}
-            Total Price of All Elevators: #{total_price_of_elevators}
-            Installation fees: #{installation_fees}
-            Total Price: #{final_price}" }, 
-            :submitter_id => client.current_user.id, 
-            :type => "task",
-            :priority => "normal")
-        puts "Ticket creation?"
-        client.tickets.save
-        puts "Ticket saved?"
-    end
+    #         # When getting the error 'hostname does not match the server certificate'
+    #         # use the API at https://yoursubdomain.zendesk.com/api/v2
+    #     end
+    #     # ticket = ZendeskAPI::Ticket.new("#{client}", :id => 1, :priority => "urgent")
+    #     puts "This is the client after creation #{client}"
+    #     client.tickets.create(:subject => "#{company_name} at #{email}", 
+    #         :comment => { :value => "The company #{company_name} which can be reached at email #{email} sent us a quote requesting Rocket Elevators services. Here's the key elements of the quote we received:
+    #         Building type: #{name}
+    #         Product Line Quality: #{product_line}
+    #         Number of Apartments: #{number_of_appartement}
+    #         Number of Floors: #{number_of_floor}
+    #         Number of Basements: #{number_of_basement}
+    #         Number of Companies: #{number_of_companies}
+    #         Number of Parking Spots: #{number_of_parking_spots}
+    #         Number of Corporations: #{number_of_corporations}
+    #         Maximum Occupancy: #{maximum_occupancy}
+    #         Total Business Hours/Day: #{business_hours}
+    #         Amount of Elevator needed: #{number_of_elevators}
+    #         Unit Price per Elevator: #{unit_price_of_each_elevator}
+    #         Total Price of All Elevators: #{total_price_of_elevators}
+    #         Installation fees: #{installation_fees}
+    #         Total Price: #{final_price}" }, 
+    #         :submitter_id => client.current_user.id, 
+    #         :type => "task",
+    #         :priority => "normal")
+    #     puts "Ticket creation?"
+    #     client.tickets.save
+    #     puts "Ticket saved?"
+    # end
 end
