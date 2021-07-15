9abd911699728a5c0cc14cc282818be954f232b8
hakimoss <hakimos@live.ca>
2021-07-15 13:26:33 -0400

    zendesk work

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -19,4 +19,70 @@ class Intervention < ApplicationRecord
         end
 
     end
+
+
+
+       after_create :ticket_create_contact
+    require 'zendesk_api'
+
+    def ticket_create_contact
+        client = ZendeskAPI::Client.new do |config|
+            # Mandatory:
+          
+            config.url = "https://abdelhakim.zendesk.com/api/v2" # e.g. https://rocketelevator8148.zendesk.com/api/v2
+          
+            # Basic / Token Authentication
+            config.username = "hakimososos@gmail.com"
+            
+          
+            # Choose one of the following depending on your authentication choice
+            config.token = ENV["ZENDESK_APIKEY"]
+          
+          #   # OAuth Authentication
+          #   config.access_token = "your OAuth access token"
+          
+            # Optional:
+          
+            # Retry uses middleware to notify the user
+            # when hitting the rate limit, sleep automatically,
+            # then retry the request.
+            config.retry = true
+          
+            # Raise error when hitting the rate limit.
+            # This is ignored and always set to false when `retry` is enabled.
+            # Disabled by default.
+            config.raise_error_when_rate_limited = false
+          
+            # Logger prints to STDERR by default, to e.g. print to stdout:
+            require 'logger'
+            config.logger = Logger.new(STDOUT)
+          
+            # Disable resource cache (this is enabled by default)
+            config.use_resource_cache = false
+          
+            # Changes Faraday adapter
+            # config.adapter = :patron
+          
+            # Merged with the default client options hash
+            # config.client_options = {:ssl => {:verify => false}, :request => {:timeout => 30}}
+          
+            # When getting the error 'hostname does not match the server certificate'
+            # use the API at https://yoursubdomain.zendesk.com/api/v2
+        end
+        # ticket = ZendeskAPI::Ticket.new("#{client}", :id => 1, :priority => "urgent")
+        customer = Customer.find(customer_id)
+        author = Employee.find(author_id)
+        employee = Employee.find(employee_id)
+
+        puts "This is the client after creation #{client}"
+        client.tickets.create(
+            :subject => "problem or question", 
+            :comment => { :value => "The author #{author.first_name} #{author.last_name} created a ticket for the company #{customer.compagny_name} for the Building ID: #{building_id}, Battery ID: #{battery_id}, Column ID: #{column_id}, Elevator ID: #{elevator_id}. The employee assigned to this task is #{employee.first_name} #{employee.last_name}" }, 
+            :submitter_id => client.current_user.id,
+            :type => "question", 
+            :priority => "normal")
+        puts "Ticket creation?"
+        client.tickets.save
+        puts "Ticket saved?"
+    end
 end
