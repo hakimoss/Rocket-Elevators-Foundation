311957e265f95455f32b632ca3699a136f83da23
hakimoss <hakimos@live.ca>
2021-07-20 09:10:16 -0400

    block account apres 4 fois et unluck apres 1h

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable, :timeoutable
+         :rememberable, :validatable, :timeoutable, :lockable
   has_one :customer
   has_one :employee
 
